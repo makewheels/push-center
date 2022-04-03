c8ca86069c0796afd307af9fd877e3ba94684bd8
makewheels <makewheels@github.com>
2022-04-04 01:15:33 +0800

    init

---
+++ src/main/java/com/github/makewheels/pushcenter/PushCenterApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.pushcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushCenterApplication.class, args);
+    }
+
+}
